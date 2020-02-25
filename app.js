67d460c13d7ab412c221bba7aaa8e12b698b686a
Hitesh Savaliya <hrsavaliya9@gmail.com>
2020-02-25 14:28:09 +0530

    server code pushed

--- app.js
+++ app.js
@@ -1,14 +1,49 @@
-const http = require('http');
+const express = require('express')
+const http = require('http')
+var io = require('socket.io')
+const app = express()
+const port = 8080
 
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+// app.get('/', (req, res) => res.send('Hello World!!!!!'))
+//https://expressjs.com/en/guide/writing-middleware.html
+app.use(express.static(__dirname + '/build'))
+app.get('/', (req, res, next) => {
+    res.sendFile(__dirname + '/build/index.html')
+})
+const server = http.createServer(app)
+const peers = io(server)
+// https://www.tutorialspoint.com/socket.io/socket.io_namespaces.htm
+// const peers = io;
+// keep a reference of all socket connections
+let connectedPeers = new Map()
+peers.on('connection', socket => {
+  console.log('444', socket.id);
+  socket.emit('connection-success', { success: socket.id })
+  connectedPeers.set(socket.id, socket)
+  socket.on('disconnect', () => {
+    console.log('disconnected')
+    connectedPeers.delete(socket.id)
+  })
+  socket.on('offerOrAnswer', (data) => {
+    // send to the other peer(s) if any
+    for (const [socketID, socket] of connectedPeers.entries()) {
+      console.log(socketID, socket);
+      // don't send to self
+      if (socketID !== data.socketID) {
+        console.log(socketID, data.payload.type)
+        socket.emit('offerOrAnswer', data.payload)
+      }
+    }
+  })
+  socket.on('candidate', (data) => {
+    // send candidate to the other peer(s) if any
+    for (const [socketID, socket] of connectedPeers.entries()) {
+      // don't send to self
+      if (socketID !== data.socketID) {
+        console.log(socketID, data.payload)
+        socket.emit('candidate', data.payload)
+      }
+    }
+  })
+})
+server.listen(port, () => console.log(`Listening on port ${port}`))
